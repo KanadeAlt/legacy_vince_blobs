e3ed96ef5c90c3f5b804e7e3ba165017b7045dcd
KanadeAlt <enashinonome264@gmail.com>
2025-11-05 18:12:42 +0000

    Import unmodified legacy fingerprint vince blobs

---
+++ extract-files.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so)
+            "${PATCHELF}" --remove-needed "libprotobuf-cpp-lite.so" "${2}"
+            ;;
+    esac
+
+}
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
+set -e
+
+export DEVICE=vince
+export DEVICE_COMMON=mithorium-common
+export VENDOR=xiaomi
+
+"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
