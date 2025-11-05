9a86619b03e46cb8599789a5fc730ea544bc9e5d
KanadeAlt <enashinonome264@gmail.com>
2025-11-05 19:47:26 +0000

    replace libstdc++.vendor => libstdc++_vendor on camera vince blobs use PATCHELF_0_18
    Yes, this is necessary because libstdc++ is not available since Android 14 and has been replaced with libstdc++_vendor.
    
    logcat :
    Could not load /vendor/lib64/libchromaflash.so from default namespace: dlopen failed: library "libstdc++.so" not found: needed by /vendor/lib64/libchromaflash.so in namespace (default).
    
    Could not load /vendor/lib/libmmcamera_hdr_gb_lib.so from default namespace: dlopen failed: library "libstdc++.so" not found: needed by /vendor/lib/libmmcamera_hdr_gb_lib.so in namespace (default).
    
    Could not load /vendor/lib/liboptizoom.so from default namespace: dlopen failed: library "libstdc++.so" not found: needed by /vendor/lib/liboptizoom.so in namespace (default).
    
    Could not load /vendor/lib/libseemore.so from default namespace: dlopen failed: library "libstdc++.so" not found: needed by /vendor/lib/libseemore.so in namespace (default).
    
    Could not load /vendor/lib/libtrueportrait.so from default namespace: dlopen failed: library "libstdc++.so" not found: needed by /vendor/lib/libtrueportrait.so in namespace (default).
    
    Could not load /vendor/lib/libubifocus.so from default namespace: dlopen failed: library "libstdc++.so" not found: needed by /vendor/lib/libubifocus.so in namespace (default).

--- extract-files.sh
+++ extract-files.sh
@@ -8,6 +8,14 @@
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib/libchromaflash.so \
+        | libmmcamera_hdr_gb_lib.so \
+        | liboptizoom.so \
+        | libseemore.so \
+        | libtrueportrait.so \
+        | libubifocus.so )
+            "${PATCHELF_0_18}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+            ;;
         vendor/lib/libmmcamera_tuning.so)
             "${PATCHELF_0_18}" --remove-needed "libmm-qcamera.so" "${2}"
             ;;
@@ -23,6 +31,11 @@ function blob_fixup() {
             "${PATCHELF_0_18}" --replace-needed "libvendor.goodix.hardware.fingerprint@1.0.so" "vendor.goodix.hardware.fingerprint@1.0.so" "${2}"
             ;;
     esac
+
+    # For all ELF files
+    if [[ "${1}" =~ ^.*(\.so|\/bin\/.*)$ ]]; then
+        "${PATCHELF_0_17_2}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+    fi
 
 }
 
